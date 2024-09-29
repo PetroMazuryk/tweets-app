b861da6a43b205dbb34bf19c728cfc9d09b195f4
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-09-30 00:22:53 +0300

    add file github and husky

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
   },
+  base: "/tweets-app/",
 });
