2123d26f57fb3646a01b24a938f85a55e31b2aa6
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-09-30 01:01:25 +0300

    add new env

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets-app">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_TEST,
 });
 
 async function fetchUsers() {
