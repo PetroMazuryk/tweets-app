8acf5da642534ce2462a8e0f86e1ae155d3b5238
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-09-30 23:13:04 +0300

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tweets-app/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
